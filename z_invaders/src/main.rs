a1e07f21da56ac0e55fda1202146030d7067ad68
Ricardo Bustamante <ricardo@busta.dev>
2022-12-19 23:23:32 +0100

    add audio and initial invaders setup

--- z_invaders/src/main.rs
+++ z_invaders/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::ptr::null;
+use rusty_audio::Audio;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+
+    audio.add("explode", "assets\\explode.wav");
+    audio.add("lose", "assets\\lose.wav");
+    audio.add("move", "assets\\move.wav");
+    audio.add("pew", "assets\\pew.wav");
+    audio.add("startup", "z_invaders/assets/startup.wav");
+    audio.add("win", "assets\\win.wav");
+    audio.play("startup");
+
+    //cleanup
+    audio.wait();
+
+    Ok(())
 }
