b0538b9e2abde2beaca6c60634a24abeccbfff68
Ricardo Bustamante <ricardo@busta.dev>
2022-12-19 08:41:07 +0100

    Add collections exercise

--- g_collections_enums/src/main.rs
+++ g_collections_enums/src/main.rs
@@ -10,6 +10,14 @@
 //
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
+use crate::Shot::{Bullseye, Hit, Miss};
+
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
+
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
@@ -18,6 +26,12 @@ impl Shot {
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
         // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
         // - return 0 points if `self` is a Miss
+        match self {
+            Bullseye => 5,
+            Hit(x) if x < 3.0 => 2,
+            Hit(x) => 1,
+            Miss => 0,
+        }
     }
 }
 
@@ -34,10 +48,25 @@ fn main() {
     //      - Less than 1.0 -- `Shot::Bullseye`
     //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
     //      - Greater than 5.0 -- `Shot::Miss`
+    for coord in arrow_coords {
+        coord.print_description();
+        let shot = match coord.distance_from_center() {
+            x if x < 1.0 => Bullseye,
+            x if x < 5.0 => Hit(x),
+            x => Miss,
+        };
+        shots.push(shot);
+    }
 
 
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
+
+    for shot in shots {
+        let points = shot.points();
+        println!("points {}", points);
+        total += points;
+    }
 
     println!("Final point total is: {}", total);
 }
@@ -60,7 +89,6 @@ impl Coord {
             self.x,
             self.y);
     }
-
 }
 
 // Generate some random coordinates
